3006b3f8036cc3d50ab523326124960d21079ff8
Guil-Baka <guilam.dev@gmail.com>
2023-09-10 14:56:32 -0300

    Commit Inicial.

---
+++ testPage/main.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <!-- autoreload page -->
+  <meta http-equiv="refresh" content="5" />
+  <!--  jesus cristo pq que tem que fazer essa merda pra incluir css no php wtf -->
+  <style>
+    <?php include './src/css/styles.css'; ?>
+  </style>
+</head>
+
+<body class="bodyTest">
+  <div>
+    <h1>Test Page</h1>
+  </div>
+
+  <?php
+  echo "Hello World";
+  echo "<br>";
+  echo "Hello World";
+  echo "<br>";
+  echo "Hello World";
+  echo "<br>";
+  ?>
+</body>
+
+</html>
